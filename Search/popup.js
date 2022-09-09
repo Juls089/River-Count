c97b5e7ba28b06aa160eed357550e1fff804e926
Juls <89405514+Juls089@users.noreply.github.com>
2022-09-09 16:39:31 +0200

    Add files via upload

---
+++ Search/popup.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('loaded page at ' + new Date().toLocaleString());
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  var content = document.getElementById('content');
+  var totalWord = request.words.length;
+  document.getElementById('messages').innerHTML = `${chrome.i18n.getMessage('wordsFound')}: <span class='ui teal circular label'>${totalWord}</span>`;
+  document.getElementById('word').textContent = chrome.i18n.getMessage('word');
+  document.getElementById('qty').textContent = chrome.i18n.getMessage('qty');
+  var tbody = document.querySelector('#word-table tbody');
+
+  request.words.forEach( (words) => {
+    var tr = document.createElement('tr');
+    var tdWord = document.createElement('td');
+    var label = document.createElement('span');
+    var tdCounter = document.createElement('td');
+    var tdPercent = document.createElement('td');
+
+    label.textContent = words.word;
+    label.classList.add('ui', 'small', 'blue', 'label');
+    tdWord.appendChild(label);
+    tdCounter.textContent = words.counter;
+    tdPercent.textContent = `${((words.counter / totalWord) * 100).toLocaleString()}%`;
+
+    tr.appendChild(tdWord);
+    tr.appendChild(tdCounter);
+    tr.appendChild(tdPercent);
+
+    tbody.appendChild(tr);
+
+  });
+});
+
+window.onload = chrome.tabs.executeScript(null, {
+    file: "treewalker.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      document.getElementById('messages').innerText = 'Fehler \n' + chrome.runtime.lastError.message;
+    }
+});
+
+
+function detectLanguage(text) {
+  chrome.i18n.detectLanguage(text, function(result) {
+
+    var outputLang = "Detected Language: ";
+    var outputPercent = "Language Percentage: ";
+
+    for(i = 0; i < result.languages.length; i++) {
+      outputLang += result.languages[i].language + " ";
+      outputPercent +=result.languages[i].percentage + " ";
+    }
+
+    document.getElementById("messages").innerHTML = outputLang + "\n" + outputPercent + "\nReliable: " + result.isReliable;
+  });
+}
---
+++ Search/treewalker.js
@@ -0,0 +1,45 @@
+var nodeReject = {
+  acceptNode: function(node) {
+    if(!node.parentNode.nodeName.match(/SCRIPT|STYLE|CODE/))
+      return NodeFilter.FILTER_ACCEPT;
+  }
+};
+
+function treeWalker(ele) {
+  var nodes = document.createTreeWalker(ele, NodeFilter.SHOW_TEXT, nodeReject, false);
+  var text = [];
+  regex = new RegExp(/^['"!@#$£%¢¬&*()\-_+=§`´{[^~\]}<,.>›·–—•;:\/°?|\d]+|['"!@#$£%¢¬&*()\-_+=§`´{[^~\]}<,.>;:\/°?|\d]+$/g);
+
+  while(node = nodes.nextNode()) {
+    var words = node.data
+                    .split(/\s/)
+                    .map((str) => str.replace(regex, ''))
+                    .filter((str) => !!str);
+
+    text = text.concat(words);
+  }
+  return text;
+}
+
+
+var nodes = treeWalker(document.body);
+
+var words = []
+
+nodes.forEach( (text) => {
+  var word = text.toLowerCase();
+
+  var wordIndex = words.findIndex( (w) => w.word == word );
+
+  if(wordIndex >= 0) {
+    words[wordIndex].counter = words[wordIndex].counter + 1;
+  }
+  else {
+    words.push({word: word, counter: 1});
+  }
+
+});
+
+words = words.sort( (a, b) => b.counter - a.counter );
+
+chrome.runtime.sendMessage({ words: words });
